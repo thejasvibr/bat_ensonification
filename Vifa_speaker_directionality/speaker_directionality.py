bd0e13149511196f87c608571cdc7f574261b26d
tbeleyur <tbeleyur@orn.mpg.de>
2016-11-18 12:35:00 +0100

    figured out how to get it working even in Fireface!

--- Vifa_speaker_directionality/speaker_directionality.py
+++ Vifa_speaker_directionality/speaker_directionality.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 FS=192 * 1000 # sampling rate
 playbackdurn=3.0 # length of playback sound
 numplaybacks=2
-silencedurn=3
+silencedurn=1
 rampduration=0.5 # length of up and down ramp in seconds
 amplification=10 # in dB
 
@@ -37,14 +37,22 @@ silence_playback=np.zeros(silencedurn*FS)
 
 unitplayback=np.hstack((windowed_noise,silence_playback))
 
-completeplayback=np.tile(unitplayback,numplaybacks)*10**(amplification/20.0)
+somesilence=np.zeros(rampsamples)
 
+completeplayback=np.hstack((somesilence,np.tile(unitplayback,numplaybacks)*10**(amplification/20.0),somesilence))
+#
+#syncsignal=np.zeros(completeplayback.size)
+#syncsignal[somesilence.size+1]=0.8 # indicate=start of signal
+
+DeviceIndex=40 # for Fireface
+
+playbackarray=(completeplayback)
 
 
 # now to initiate sounddevice for the simultaneousl recording and playback:
-print('recording and playback intiated....')
-recordedsound=sd.playrec(completeplayback,samplerate=FS,channels=1,blocking=True)
-print('recording and playback stopped....')
+print('\n recording and playback intiated....')
+recordedsound=sd.playrec(completeplayback,samplerate=FS,output_mapping=[1],input_mapping=[3,9],blocking=True,device=DeviceIndex)
+print('\n recording and playback stopped....')
 #
 #deviceid=40 # 40 for the Fireface ASIO USB
 
@@ -63,10 +71,12 @@ print('recording and playback stopped....')
 
 if __name__=='__main__':
 
+
     plt.figure(1)
     fig=plt.subplot(111)
     fig.set_xlim(xmin=0,xmax=unitplayback.size)
     fig.set_ylim(ymin=-1,ymax=1)
+    print('\n plots are being plotted')
     plt.plot(unitplayback)
 
 
