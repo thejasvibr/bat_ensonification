7737632a86bfd0c16574028c3e8933e39a6e4477
tbeleyur <tbeleyur@orn.mpg.de>
2016-11-25 14:37:12 +0100

    findrecordingdelay working well - the trick is to not give a very large number of samples to be considered

--- Vifa_speaker_directionality/noise_analyses.py
+++ Vifa_speaker_directionality/noise_analyses.py
@@ -11,6 +11,7 @@ of each chunk as a separate line
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
+import scipy.signal
 from scipy.io import wavfile
 
 def findsyncpulse(syncsignal):
@@ -84,17 +85,13 @@ def findrecordingdelay(recordedsig,internalsig,startsilence,playbacksamples,spea
 
     delaysamples=int(np.around(traveltime*FS))
 
-    recfirstplayback=recordedsig[:delaysamples+playbacksamples+startsilence]
+    recfirstplayback=recordedsig[:playbacksamples+delaysamples+startsilence]
     internalfirstplayback=internalsig[:recfirstplayback.shape[0]]
-    sigcor=np.correlate(recfirstplayback,internalfirstplayback,mode='valid')
+    sigcor=np.correlate(recfirstplayback,internalfirstplayback,mode='same')
 
-    lagindex=np.argmax(sigcor)
+    lagindex=  np.argmax(sigcor) - sigcor.shape[0]/2.0
 
-    return(lagindex)
-
-
-
-
+    return(int(lagindex) )
 
 
 
@@ -193,50 +190,66 @@ if __name__== '__main__':
     delaysamples=int(np.around((spkr2micdist/vsound)*FS))
 
 
-    testplayback=np.hstack((startsilence,noissig ))
+    testplayback=np.hstack((startsilence,noissig,np.zeros(delaysamples) ))
     recordedplayback=np.hstack((  startsilence,np.zeros(delaysamples),noissig ))
+    noisyrecordedplayback=np.hstack((  startsilence,np.zeros(delaysamples),noissig )) + np.random.normal(0,0.01,recordedplayback.shape[0])
     sigcor=np.correlate(recordedplayback,testplayback,mode='same')
 
     #findrecordingdelay(recordedsig,internalsig,startsilence,playbacksamples,speaker2micdist,vsound,FS)
 
     delayindex=findrecordingdelay(recordedplayback,testplayback,startsilence.shape[0],noissig.shape[0],spkr2micdist,vsound,samplerate)
 
+    # checking the correlation with a noisy signal too !!
+    noisydelayindex=findrecordingdelay(noisyrecordedplayback,testplayback,startsilence.shape[0],noissig.shape[0],spkr2micdist,vsound,samplerate)
 
 
-    # testing extractplaybacks:
-    noisesig=np.random.normal(0,1,100)
-    silence=np.zeros(100)
-    numplaybacks=5
+    # testing it with real recordings :
+    hp=1000
+    a,b=scipy.signal.butter(8,hp/192000.0,'high')
+    hp_sound=scipy.signal.lfilter(a,b,recsound[1])
 
-    intsignal=np.hstack( (noisesig,silence ) )
-    intrec=np.tile(intsignal,numplaybacks)
-    recsign=np.hstack(  (silence,noisesig ) )
-    micrec=np.tile(recsign,numplaybacks)
+    reallagindex=findrecordingdelay(hp_sound,recsound[0],288000,576000,0.375,340,192000)
 
-    pbksamples=noisesig.shape[0]
-    silencesampls=silence.shape[0]
-
-
-
-    #extractplaybacks(internalsig,recsig,playbacksamples,silencesamples,numplaybacks,spearkermicdist,FS,vsound)
-
-    #k=extractplaybacks(intrec,micrec,pbksamples,silencesampls,numplaybacks,0.4,192000,340)
-
-
-    # take signal only from postpulse
     syncindex=findsyncpulse(recsound[2])
-
     internalsig=signalpostpulse(recsound[0],syncindex)
-    recsig=signalpostpulse(recsound[1],syncindex)
 
-    recplaybacks=extractplaybacks(internalsig,recsig,playbacksamples,silencesamples,numplaybacks,0.375,192000,340)
-
-
-    q=fftpack.fft(recsound[0])
-    r=fftpack.fft(recsound[1])
-    qr=-q.conjugate()
-    rr=-r.conjugate()
-
-    np.argmax(np.abs(fftpack.ifft(rr*qr)))
-
+    hp_recsig=signalpostpulse(hp_sound,syncindex)
+    postpulse_lagindex=findrecordingdelay(hp_recsig,internalsig,0,576000,0.375,340,192000)
+#
+#    # testing extractplaybacks:
+#    noisesig=np.random.normal(0,1,100)
+#    silence=np.zeros(100)
+#    numplaybacks=5
+#
+#    intsignal=np.hstack( (noisesig,silence ) )
+#    intrec=np.tile(intsignal,numplaybacks)
+#    recsign=np.hstack(  (silence,noisesig ) )
+#    micrec=np.tile(recsign,numplaybacks)
+#
+#    pbksamples=noisesig.shape[0]
+#    silencesampls=silence.shape[0]
+#
+#
+#
+#    #extractplaybacks(internalsig,recsig,playbacksamples,silencesamples,numplaybacks,spearkermicdist,FS,vsound)
+#
+#    #k=extractplaybacks(intrec,micrec,pbksamples,silencesampls,numplaybacks,0.4,192000,340)
+#
+#
+#    # take signal only from postpulse
+#    syncindex=findsyncpulse(recsound[2])
+#
+#    internalsig=signalpostpulse(recsound[0],syncindex)
+#    recsig=signalpostpulse(recsound[1],syncindex)
+#
+#    recplaybacks=extractplaybacks(internalsig,recsig,playbacksamples,silencesamples,numplaybacks,0.375,192000,340)
+#
+#
+#    q=fftpack.fft(recsound[0])
+#    r=fftpack.fft(recsound[1])
+#    qr=-q.conjugate()
+#    rr=-r.conjugate()
+#
+#    np.argmax(np.abs(fftpack.ifft(rr*qr)))
+#
 
