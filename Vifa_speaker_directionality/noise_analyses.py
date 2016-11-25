7e9e773385a74aa6bee3d7c3907c531f302c2033
tbeleyur <tbeleyur@orn.mpg.de>
2016-11-25 15:02:11 +0100

    added new function - highpassfilter

--- Vifa_speaker_directionality/noise_analyses.py
+++ Vifa_speaker_directionality/noise_analyses.py
@@ -50,27 +50,18 @@ def signalpostpulse(recsound,syncindex):
     return(cutsound)
 
 
+
 def calculateIR(originalsignal,recsignal):
 
 
     pass
 
-def calcmaxcorr(recsig,internalsig):
-
-    # scan one second of signal + delay
-    crosscor=np.correlate(recsig,internalsig,'same')
-    lagindex= np.argmax(crosscor)         #np.where(crosscor==max(crosscor))[0][0]
-    delayindex=lagindex - np.around(np.shape(internalsig)[0]/2.0)
-    if delayindex<0:
-        print('the recorded signal begins before the internal signal %d'%delayindex)
-
-    else :
-        return(delayindex)
 
 
 def findrecordingdelay(recordedsig,internalsig,correlnlength,speaker2micdist,vsound,FS):
     '''
     finds the time lag in number of samples between the start of speaker playback and mic recording
+    Note: the function works reasonably fast if the correlnlengths are kept <100000 samples
 
     Inputs:
         recordedsig: np.array. the array with mike recordings
@@ -90,18 +81,21 @@ def findrecordingdelay(recordedsig,internalsig,correlnlength,speaker2micdist,vso
     sigcor=np.correlate(recfirstplayback,internalfirstplayback,mode='same')
 
     lagindex=  np.argmax(sigcor) - sigcor.shape[0]/2.0
+    if lagindex <=0:
+        print('error in the cross-correlation. Mic recording begins before the internal signal')
+    else:
 
-    return(int(lagindex) )
+        return(int(lagindex) )
+
+def highpassfilter(signal,hpfreq,FS):
+
+    a,b=scipy.signal.butter(8,hpfreq/FS,'high')
+    hp_signal=scipy.signal.lfilter(a,b,signal)
+
+    return(hp_signal)
 
 
-
-
-
-
-
-
-
-def extractplaybacks(internalsig,recsig,playbacksamples,silencesample,delayindex,numplaybacks):
+def extractplaybacks(recsig,playbacksamples,silencesample,delayindex,numplaybacks):
     '''
     cuts out the noise playbacks and assigns them as separate np.arrays
     Input:
@@ -115,9 +109,6 @@ def extractplaybacks(internalsig,recsig,playbacksamples,silencesample,delayindex
     recplaybacks: np.array w numplaybacks x playbackduration  dimensions. each row has one recorded playback
 
     '''
-
-
-    delayindex=calcmaxcorr(recsig,internalsig,FS,spearkermicdist,vsound)
 
     recplaybacks=np.zeros((playbacksamples,numplaybacks))
 
@@ -190,17 +181,17 @@ if __name__== '__main__':
     delaysamples=int(np.around((spkr2micdist/vsound)*FS))
 
 
-    testplayback=np.hstack((startsilence,noissig,np.zeros(delaysamples) ))
-    recordedplayback=np.hstack((  startsilence,np.zeros(delaysamples),noissig ))
-    noisyrecordedplayback=np.hstack((  startsilence,np.zeros(delaysamples),noissig )) + np.random.normal(0,0.01,recordedplayback.shape[0])
+    testplayback=np.hstack((noissig,np.zeros(delaysamples) ))
+    recordedplayback=np.hstack((np.zeros(delaysamples),noissig ))
+    noisyrecordedplayback=np.hstack((np.zeros(delaysamples),noissig )) + np.random.normal(0,0.01,recordedplayback.shape[0])
     sigcor=np.correlate(recordedplayback,testplayback,mode='same')
 
     #findrecordingdelay(recordedsig,internalsig,startsilence,playbacksamples,speaker2micdist,vsound,FS)
 
-    delayindex=findrecordingdelay(recordedplayback,testplayback,startsilence.shape[0],noissig.shape[0],spkr2micdist,vsound,samplerate)
+    delayindex=findrecordingdelay(recordedplayback,testplayback,1000,spkr2micdist,vsound,samplerate)
 
     # checking the correlation with a noisy signal too !!
-    noisydelayindex=findrecordingdelay(noisyrecordedplayback,testplayback,startsilence.shape[0],noissig.shape[0],spkr2micdist,vsound,samplerate)
+    noisydelayindex=findrecordingdelay(noisyrecordedplayback,testplayback,2000,spkr2micdist,vsound,samplerate)
 
 
     # testing it with real recordings :
@@ -208,48 +199,13 @@ if __name__== '__main__':
     a,b=scipy.signal.butter(8,hp/192000.0,'high')
     hp_sound=scipy.signal.lfilter(a,b,recsound[1])
 
-    #reallagindex=findrecordingdelay(hp_sound,recsound[0],288000,576000,0.375,340,192000)
-
     syncindex=findsyncpulse(recsound[2])
     internalsig=signalpostpulse(recsound[0],syncindex)
 
     hp_recsig=signalpostpulse(hp_sound,syncindex)
-    postpulse_lagindex=findrecordingdelay(hp_recsig,internalsig,10000,0.375,340,192000)
+    postpulse_lagindex=findrecordingdelay(hp_recsig,internalsig,100000,0.375,330,192000)
 #
-#    # testing extractplaybacks:
-#    noisesig=np.random.normal(0,1,100)
-#    silence=np.zeros(100)
-#    numplaybacks=5
-#
-#    intsignal=np.hstack( (noisesig,silence ) )
-#    intrec=np.tile(intsignal,numplaybacks)
-#    recsign=np.hstack(  (silence,noisesig ) )
-#    micrec=np.tile(recsign,numplaybacks)
-#
-#    pbksamples=noisesig.shape[0]
-#    silencesampls=silence.shape[0]
-#
-#
-#
-#    #extractplaybacks(internalsig,recsig,playbacksamples,silencesamples,numplaybacks,spearkermicdist,FS,vsound)
-#
-#    #k=extractplaybacks(intrec,micrec,pbksamples,silencesampls,numplaybacks,0.4,192000,340)
-#
-#
-#    # take signal only from postpulse
-#    syncindex=findsyncpulse(recsound[2])
-#
-#    internalsig=signalpostpulse(recsound[0],syncindex)
-#    recsig=signalpostpulse(recsound[1],syncindex)
-#
-#    recplaybacks=extractplaybacks(internalsig,recsig,playbacksamples,silencesamples,numplaybacks,0.375,192000,340)
-#
-#
-#    q=fftpack.fft(recsound[0])
-#    r=fftpack.fft(recsound[1])
-#    qr=-q.conjugate()
-#    rr=-r.conjugate()
-#
-#    np.argmax(np.abs(fftpack.ifft(rr*qr)))
-#
+############# extractplaybacks(recsig,playbacksamples,silencesample,delayindex,numplaybacks)
+
+    allpbks=extractplaybacks(hp_recsig,576000,288000,220,19)
 
