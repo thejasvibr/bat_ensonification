6dc90eb322575ffa4ec9b52361595fce91e58009
thejasvibr <thejasvib@gmail.com>
2017-01-31 12:38:06 +0100

    separate out playback array folder and file names

---
+++ playback_experiment_script.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Bat ensonification experiment - Grossflugraum 
+script that initiates playbacks and saves the resulting recording
+Created on Wed Jan 25 10:25:21 2017
+
+@author: tbeleyur
+"""
+import scipy.io.wavfile as wav
+import numpy as np 
+import matplotlib.pyplot as plt 
+import playback_saving_funcs as bat_enson
+import sounddevice as sd 
+import datetime as dt 
+plt.rcParams['agg.path.chunksize'] = 10000
+
+
+# location of playback sound file  
+pbk_wav_locn = 'C://Users//tbeleyur//Desktop//ensonification_data//2017_01_25//playback_sound//'
+pbk_file = 'cIR_conv_signal_2017-01-31_11-16'
+
+
+# initiate and record playback :
+in_channels = [2,9] # [sync, microphone]
+out_channels = [2,1] # [ sync , speaker ] 
+ai_number = 38 # audio interface serial number 38 for PC opp Fluraum klein, and 40 for TBR laptop
+
+# user input required :
+# target file name :
+playback_angle = 0
+rec_type = 'with_' # with_ or without_ bat
+tgt_folder = 'C://Users//tbeleyur//Desktop//ensonification_data//2017_01_25//'
+
+# script puts the name together 
+time_stamp = dt.datetime.now().strftime('%Y-%m-%d_%H_%S')
+file_name = 'playback_angle_%d_'%playback_angle + rec_type + time_stamp + '.WAV'
+complete_file = tgt_folder + file_name
+
+
+# load the playback sound file :
+FS, pbk_sound = bat_enson.load_playback_sound(pbk_wav_locn)
+
+composite_playback = bat_enson.include_sync_signal(pbk_sound)
+
+rec_sound = sd.playrec(composite_playback, samplerate = FS, 
+   input_mapping = in_channels, output_mapping= out_channels, device = ai_number)
+sd.wait()
+
+plt.plot(rec_sound)
+
+rec_post_sync = bat_enson.remove_pre_sync(rec_sound)
+
+bat_enson.save_rec_file(rec_post_sync,FS,complete_file)
+bat_enson.save_rec_file(np.float16(rec_post_sync),FS,complete_file)
+
