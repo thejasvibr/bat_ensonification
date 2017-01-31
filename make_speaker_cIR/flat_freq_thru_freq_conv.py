eeb613cbf5dd0bc9324e87281a560f2c804b13d5
Thejasvi Beleyur <thejasvibr@users.noreply.github.com>
2017-01-31 12:03:05 +0100

    Add files via upload
    upload from tierhaus

--- make_speaker_cIR/flat_freq_thru_freq_conv.py
+++ make_speaker_cIR/flat_freq_thru_freq_conv.py
@@ -6,7 +6,6 @@ Created on Mon Jan 30 09:51:12 2017
 
 @author: tbeleyur
 """
-
 import numpy as np
 import sounddevice as sd
 import matplotlib.pyplot as plt
@@ -18,10 +17,18 @@ from scipy.interpolate import InterpolatedUnivariateSpline
 plt.rcParams['agg.path.chunksize'] = 10000
 import scipy.io.wavfile as wav
 import calc_cIR as ir_funcs
+import sys 
+import datetime as dt
+sys.stdout.flush()
 
 np.random.seed(612)
 
-durn = 1.5
+# location to where the generated data is saved to as numpy arrays
+target_folder = 'C:\\Users\\tbeleyur\\Desktop\\ensonification_data\\2017_31_01_playback_sound\\'
+
+
+# playback and recording details :
+durn = 3.0
 FS = 192000
 num_samples = int(durn*FS)
 ramp_durn = 0.1
@@ -69,8 +76,10 @@ pbk_sync = np.argmax(abs(orig_playback[:,0]))
 post_sync_orig = orig_playback[pbk_sync:,1]
 post_sync_orig = post_sync_orig[:post_sync_rec.size]
 
-align_cor = np.correlate(post_sync_rec,post_sync_orig,'same')
 print('signal correlation happening now')
+align_cor = np.correlate(post_sync_rec,post_sync_orig,'same')
+
+print('')
 
 mid_point_cor = align_cor.size/2 -1
 align_index =  np.argmax(abs(align_cor)) - mid_point_cor
@@ -90,7 +99,7 @@ conv_freq = comp_freq*orig_freq
 conv_sig = spyfft.ifft(conv_freq).real
 
 plt.figure(2)
-plt.title('Power spectrum of digital signal, un-compensated recordings AND compensated frequencies ')
+plt.title('Spectra: Original signal, recorded signal, compensatory signal  ')
 plt.xlabel('Frequency, KHz')
 plt.ylabel('Power, dB')
 
@@ -102,30 +111,28 @@ comp_freqs, = plt.plot(np.linspace(0,96,comp_freq.size/2),20*np.log10(abs(comp_f
 plt.legend(handles = [digital_sig,uncomp_rec,comp_freqs])
 
 ramp_convsig = ir_funcs.add_ramps(ramp_samples,conv_sig)
-sync_ch_convsig = np.zeros(ramp_convsig)
+sync_ch_convsig = np.zeros(ramp_convsig.size)
 sync_ch_convsig[0] = 1
 
 zerop_convsig = ir_funcs.zero_pad(silence_samples,ramp_convsig)
 zerop_sync_ch_convsig = ir_funcs.zero_pad(silence_samples,sync_ch_convsig)
 
-rms_norm_convsig = np.std(ramp_orig)/np.std(zerop_convsig) * zerop_convsig
+hp_convsig = signal.lfilter(hp_b,hp_a,zerop_convsig)
+rms_norm_convsig = np.std(orig_zeropad)/np.std(hp_convsig) * zerop_convsig
 
-hp_convsig = signal.lfilter(hp_b,hp_a,rms_norm_convsig)
 
-conv_playback = np.column_stack((zerop_sync_ch_convsig,hp_convsig))
+conv_playback = np.column_stack((zerop_sync_ch_convsig,rms_norm_convsig))
 
 
 print('compensated playback occuring now')
 # rms_norm_convsig
 conv_rec = sd.playrec(conv_playback ,samplerate=FS,input_mapping=dev_in_ch,output_mapping=dev_out_ch,device=tgt_ind)
 sd.wait()
-plt.figure(1)
-plt.plot(conv_rec)
 
 # plot the convolved playback recording taking into account the time
 # required for sound to reach after playback
 
-pbk_delay =np.argmax(conv_playback[:,0])
+pbk_delay =np.argmax(abs(conv_playback[:,0]))
 total_delay = pbk_delay + trans_delay_samples
 
 
@@ -133,7 +140,7 @@ total_delay = pbk_delay + trans_delay_samples
 plt.figure(3)
 plt.title('Power spectrum of digital signal and speaker IR compensated signal')
 plt.xlabel('Frequency, KHz')
-plt.ylabel('Power, dB')
+plt.ylabel('Power, dB (rel. max dB value)')
 
 fft_convrec = ir_funcs.get_pwr_spec( conv_rec[total_delay:total_delay+num_samples] )
 freqs_convrec = np.linspace(0,96,fft_convrec.size)
@@ -146,12 +153,26 @@ new_freqs = np.linspace(20,96,76)
 intp_val_convrec = ir_funcs.oned_fft_interp(new_freqs,freqs_convrec,fft_convrec,'linear')
 intp_val_orig = ir_funcs.oned_fft_interp(new_freqs,freqs_orig,fft_orig,'linear')
 
-convrec_plot, = plt.plot(new_freqs,intp_val_convrec-np.max(intp_val_convrec),'g*-',label ='speaker IR compensated')
-digital_sig, = plt.plot(new_freqs,intp_val_orig-np.max(intp_val_orig),'r*-',label='digital signal')
-plt.legend(handles = [convrec_plot,digital_sig])
+convrec_plot, = plt.plot(new_freqs,intp_val_convrec-np.max(intp_val_convrec),'y*-',label ='speaker IR compensated')
+digital_sig, = plt.plot(new_freqs,intp_val_orig-np.max(intp_val_orig),'b*-',label='digital signal')
+plt.legend(handles = [convrec_plot,digital_sig],bbox_to_anchor=(0.5,0.2),loc=1,borderaxespad=0.)
+
+plt.figure(1)
+plt.plot(conv_rec,label='convolved recording')
+
+plt.figure(4)
+window_length = 2048
+ir_max_point = np.argmax(abs(align_cor))
+plt.title('Impulse Response centered on max corr point : %d sample window length'%window_length)
+plt.plot(align_cor[ir_max_point-window_length/2:ir_max_point+window_length/2])
 
 
+# column stack the digital playback signal and the compensated signal for future reference :
+time_stamp = dt.datetime.now().strftime('%Y-%m-%d_%H-%M') 
 
+np.save(target_folder+'wcIRplayback_recording_' + time_stamp,conv_rec)
+np.save(target_folder+'cIR_conv_signal_'+time_stamp,rms_norm_convsig)
+np.save(target_folder+'orig_signal_'+time_stamp,orig_sig)
 
 
 
