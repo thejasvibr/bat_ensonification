4dcfbb91562a16c2435d98f76364d0f74bef4b3f
thejasvibr <thejasvib@gmail.com>
2017-01-31 16:54:15 +0100

    do bitrate calculation sensibly - saving of arrays into int16 wav files works

--- playback_saving_funcs.py
+++ playback_saving_funcs.py
@@ -27,19 +27,7 @@ def load_playback_array(sound_array):
     playback_array = np.load(sound_array)
     
     return(playback_array)
-    
-def find_device_index(tgt_dev_name):
-    '''
-    searches for device_name in 
-    the devices and detects the device index number
-    Input: string. 
-    Output: integer
-    '''
-    device_list = sd.query_devices()
-    tgt_dev_bool = [tgt_dev_name in each_device['name'] for each_device in device_list]
-    tgt_ind = int(np.argmax(np.array(tgt_dev_bool)))
-    
-    return(tgt_ind)
+ 
     
 def include_sync_signal(pbk_sound):
     '''
@@ -66,13 +54,47 @@ def remove_pre_sync(rec_sound):
 
 def save_rec_file(rec_sound,FS,tgt_file):
     '''
-    saves the rec_sound into the tgt_file.
+    saves the rec_sound into the tgt_file as an int16 WAV file.
+    Even if rec_sound is a float array it gets converted into a int16 format
+    '''
+    wav2location = lambda tgt_file,FS,array: wav.write(tgt_file,FS,array)
+        
+    if 'float' in str(rec_sound.dtype):
+        try:
+            bitrate = 16 
+            max_val_int = -1 + 2**(bitrate-1 )  
+            int16rec_sound = np.int16(rec_sound*max_val_int)
+            print('conversion from float to int happened succesfully')
+            
+            wav2location(tgt_file,FS,int16rec_sound)
+            print('saving succesfult to %s'%tgt_file)
+            return(int16rec_sound)
+                 
+            
+        except:
+            print('unsuccessful float to in16 conversion')
+        
+        
+        
+        
+    else:
+        print('integer arrays not supported - please save directly with scipy.io.wav')
+    
+
+   
+def find_device_index(tgt_dev_name):
+    '''
+    searches for device_name in 
+    the devices and detects the device index number
+    Input: string
+    Output: integer
     '''
     try:
-        wav.write(tgt_file,FS,rec_sound)
-        print('%s saved succesfully to location'%tgt_file)
+        device_list = sd.query_devices()
+        tgt_dev_bool = [tgt_dev_name in each_device['name'] for each_device in device_list]
+        tgt_ind = int(np.argmax(np.array(tgt_dev_bool)))
+        
+        return(tgt_ind)  
     except:
-        print('file saving not succesful - please check entries')
-
-    
-    
+        print('error in getting the device index number')
+        
